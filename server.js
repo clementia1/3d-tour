1660f93b675ab86f032d3aceaa802ec2f03e3ff7
clementia1 <grininma0@gmail.com>
2019-08-19 11:42:09 +0300

    webpack setup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.listen(app.get('port'), function () {
     console.log('App is running on port', app.get('port'));
 });
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src');
+let index = APP_DIR + '/index.js';
+
+module.exports = {
+    entry: {
+        main: ['babel-polyfill', index]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: BUILD_DIR,
+    },
+    module: {
+        rules: [{
+                test: /(\.css|.scss)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            },
+            {
+                test: /\.(jsx|js)?$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['env'],
+                        plugins: ['transform-class-properties', 'transform-object-rest-spread']
+                    }
+                }]
+            },
+            {
+                test: /\.(woff2?|ttf|otf|eot|svg|png)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: './static/[name].[ext]'
+                    }
+                }],
+            }
+        ],
+    }
+};
